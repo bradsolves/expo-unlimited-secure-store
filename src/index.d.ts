e0f920c645a347c6638f51a90fe6aee2b4f77442
bradsolves <bradsolves@gmail.com>
2022-05-02 12:22:19 -0400

    Initial Commit

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+import { keychainAccessibilityConstant} from 'expo-secure-store';
+declare module "@neverdull-agency/expo-unlimited-secure-store" {
+    export interface Options {
+      replacementCharacter?: string;
+      replacer?: (key: string, replacementCharacter: string) => string;
+      keychainService?: string;
+      keyChainAccessibilityConstant?: KeychainAccessibilityConstant;
+    }
+
+    export interface ExpoUnlimitedSecureStore {
+      setItem(key: string, value: string): Promise<void>;
+
+      getItem(key: string): Promise<string | null>;
+
+      removeItem(key: string): Promise<void>;
+    }
+
+    export default function(options?: Options): ExpoUnlimitedSecureStore;
+  }
